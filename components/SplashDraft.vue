be56112f46d77e58de2cb9a7b71a97bfbfd5cf2a
tab-oo <74469541+tab-oo@users.noreply.github.com>
2021-12-10 18:27:12 +0100

    Update SplashDraft.vue

--- components/SplashDraft.vue
+++ components/SplashDraft.vue
@@ -67,13 +67,28 @@
       </div>
       <div v-if="$i18n.locale == 'de'" class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <p class="mt-3 text-gray-800">
-          <em>A thousand channels</em> ist ein Softwareprojekt, mit dem wir seit September eine queere Mappingplattform entwickeln. Mit einer „Map to go“ möchten wir eine einfache Möglichkeit anbieten, eigene queere Narrationen geografisch zu visualisieren und zu veröffentlichen.
+          <em>"A thousand channels" ist ein Softwareprojekt. Wir entwicklen seit September 2021 eine queere Mappingplattform. Wir wollen damit eine leicht zugängliche Möglichkeit schaffen, eigene queere Narrationen auf einer Karte zu visualisieren und zu veröffentlichen. 
+               Die vielfältigen sozialen und politischen Aktivitäten queerer Menschen sind meist flüchtig, temporär und kein selbstverständlicher Teil des öffentlichen Bildes einer Stadt. Wir wollen eine Plattform entwickeln, um queeren Akteur*innen und Gruppen eine Möglichkeit zu bieten, von diesen Räumen und Ereignissen zu berichten.
         </p>
         <p class="mt-3 text-gray-800">
-          Habt Ihr Interesse an Kollaboration und Austausch über Coding und Design in queeren Kontexten? Oder ein wenig Zeit für Code Reviews, Übersetzungen und Akzeptanz Testing? Wir freuen uns über Unterstützung und Feedback :)
+          <b>Mitmachen</b>
+               Ihr möchtet queere Geschichten erzählen und kartieren? Ausgehend von Orten, Events, Gruppen, Aktionen oder auch Menschen?
+               Mit „A 1000 channels“ wollen wir Euch eine Möglichkeit bieten, queere Geschichte(n) zu erzählen und zu dokumentieren. Dies können die queere Kneipenkultur in einer bestimmten Stadt oder während einer bestimmten Zeit sein, Aktionen, Interventionen, Demonstrationen einer politischen Bewegung oder auch persönlicher Geschichten, Erlebnisse eines oder mehrerer Menschen sein. Oder auch die Sichtbarmachung von imaginären Orten und spekulativen Zukünften, wie wir uns unser queeres Leben vorstellen.
         </p>
         <p class="mt-3 text-gray-600">
-          <em>A thousand channels</em> wird vom Prototype Fund unterstützt :)
+          <b>Ein Baukasten</b>
+             Mit der Plattform „A 1000 channels“ stellen wir einen Baukasten zur Verfügung, mit dem Ihr verschiedene Inhalte und Medien zusammentragen, kombinieren und untereinander, räumlich und zeitlich in Bezug setzen könnt.
+             Durch Klick auf eine weltweite Karte, könnt Ihr Orte oder Ereignisse anlegen. Textfelder, der Upload von Bildern, von Audio oder Video  geben Euch verschiedene Möglichkeiten, wie Ihr von den Orten, Ereignissen und den Menschen, die diese erschaffen, erzählen wollt.
+             Wir werden bei dem Design der Tools besondere Rücksicht auf den Datenschutz nehmen, Bilder werden automatisch von Metadaten befreit, wir experimentieren mit einer Rasterung von Fotos zur Untkenntlichmachung von Gesichtern und es soll eine Funktion geben, genaue Ortsangaben zu verschleiern.
+             Am Ende eines jeden Projektes soll die Idee einer „Map to go“ stehen, eine Karte zum Mitnehmen. Damit wollen wir eine dauerhafte Abhängigkeit von unserer Platform (und unserem Projekt) auflösen. Ihr könnt ihr Eure Karte einfach mitnehmen und auf Eurer Website, Eurem eigenen Server veröffentlichen, ohne dass ihr dafür Software installieren müßt.
+        </p>
+        </p>
+        <p class="mt-3 text-gray-600">
+          <b>Feedback</b>
+             Wenn Ihr eine Idee für queere Mappings habt, meldet Euch gerne bei uns.
+             Aktuell suchen wir für Januar und Februar 2022 noch Menschen mit Ideen für Mappings, die das, woran wir gerade arbeiten, in der Praxis ausprobieren.
+             Ihr habt Interesse an Kollaboration und Austausch über Coding und Design in queeren Kontexten? Oder ein wenig Zeit für Code Reviews, Übersetzungen und Akzeptanz Testing? Wir freuen uns über Unterstützung :)
+             Euer Feedback hilft uns, die Software zu verbessern
         </p>
         <p class="mt-3 text-gray-600">
           Besucht uns <a href="https://github.com/a-thousand-channels/" class="text-link">bei Github</a> oder sendet uns eine E-Mail an
@@ -91,7 +106,7 @@
         <p class="mt-3 text-gray-800">
           <em>... Texto siguiente ...</em>
         </p>
-
+      
       </div>
       <div class="flex justify-center p-4 m-2 mb-8 space-x-2">
         <a href="https://www.a-thousand-channels.xyz/" target="_blank" class="text-link">
