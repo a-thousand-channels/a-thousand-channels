7ed1528d595b32197eadb9f0efa87bf5ed967be6
ut <ut@users.noreply.github.com>
2022-01-14 16:52:47 +0000

    deploy: 2dde9907a4e49f8ea162ae4e2ccbca49132d256d

---
+++ _nuxt/static/1642179166/en/draft/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642179166",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fdraft",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"766ac6a5"}}}}(null));
---
+++ _nuxt/static/1642179166/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642179166",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"766ac6a5"}}}}(null,"\u002F"));
