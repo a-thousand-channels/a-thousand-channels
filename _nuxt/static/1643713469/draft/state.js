d90f1865e251362d92d2bc37cf3b7e6aa54a5f23
tab-oo <tab-oo@users.noreply.github.com>
2022-02-02 16:56:41 +0000

    deploy: de9edd1fa34164037e99d47991221a3d217e6103

--- _nuxt/static/1643713469/draft/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643713469",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdraft",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d0f9413a"}}}}(null));
