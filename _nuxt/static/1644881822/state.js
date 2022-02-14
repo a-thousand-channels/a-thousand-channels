eccf49924e747056ef2d9e3cee3080a04ac1c69f
tab-oo <tab-oo@users.noreply.github.com>
2022-02-14 23:50:46 +0000

    deploy: c6b9f58f460298db042d9224043b6c742e4dec58

--- _nuxt/static/1644881822/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644881822",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fdef5208"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1644882643/en/draft/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644882643",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fdraft",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a4fe76bd"}}}}(null));
