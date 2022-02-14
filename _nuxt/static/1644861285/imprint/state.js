5ba03874620b274e2afd851748c51a3e689bd5d2
tab-oo <tab-oo@users.noreply.github.com>
2022-02-14 18:02:42 +0000

    deploy: 5d7b5f39f67037171542e844a0f11e4a2280e311

--- _nuxt/static/1644861285/imprint/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644861285",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimprint",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2be60136"}}}}(null));
