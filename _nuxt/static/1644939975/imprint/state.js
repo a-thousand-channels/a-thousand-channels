aa2ae979af75016d7f7ca313fe5198417e84a5f4
ut <ut@users.noreply.github.com>
2022-02-15 17:15:30 +0000

    deploy: 3eaa2369d80b75d5ffce5371cb8667d0c6c5724e

--- _nuxt/static/1644939975/imprint/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644939975",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimprint",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3b240f07"}}}}(null));
