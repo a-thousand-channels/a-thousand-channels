7517d5c1379349bed4a38e4d9c1ba180b3a83ef5
Jarami110d <Jarami110d@users.noreply.github.com>
2022-02-21 12:11:02 +0000

    deploy: 8edb8cb5c8a182b3aebc46d090102dfbd4b6e80d

--- _nuxt/static/1645445392/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Far\u002Fdraft","\u002Fen\u002Fdraft","\u002Fes\u002Fdraft","\u002Fdraft","\u002Fprivacy","\u002Fimprint","\u002Far\u002Fprivacy","\u002Far\u002Fimprint","\u002Fen\u002Fprivacy","\u002Fen\u002Fimprint","\u002Fes\u002Fimprint","\u002Fes\u002Fprivacy","\u002Far","\u002Fen","\u002Fes"]})
