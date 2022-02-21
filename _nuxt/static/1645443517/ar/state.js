ae964e61e5eaeeadc33c84d151524315b3219d74
Jarami110d <Jarami110d@users.noreply.github.com>
2022-02-21 11:38:40 +0000

    deploy: 81184e8bdf8186a465a2db07cdfd810c262383e9

---
+++ _nuxt/static/1645443517/ar/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645443517",layout:"default",error:a,serverRendered:true,routePath:"\u002Far",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0e7ff340"}}}}(null));
