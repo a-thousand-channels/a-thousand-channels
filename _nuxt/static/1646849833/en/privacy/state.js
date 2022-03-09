ffd04ce381cfe6bc3c4db9b29f2bcce7bdd9101b
tab-oo <tab-oo@users.noreply.github.com>
2022-03-09 18:24:18 +0000

    deploy: e8d71236fb458029cfe629e556ab65b381200253

--- _nuxt/static/1646849833/en/privacy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646849833",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fprivacy",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"60720bc3"}}}}(null));
