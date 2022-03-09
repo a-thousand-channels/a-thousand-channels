ef0bf3b87f1d40b9fc998d4cc781cc10bc97bf1c
Jarami110d <Jarami110d@users.noreply.github.com>
2022-03-09 01:24:13 +0000

    deploy: 5e7fbf3bb6427f28964f34902215e2649fb1f93c

---
+++ _nuxt/static/1646789049/imprint/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646789049",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimprint",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"226b992a"}}}}(null));
---
+++ _nuxt/static/1646789049/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646789049",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"226b992a"}}}}(null,"\u002F"));
