0f6d1a7574531546334babf93223ac521399579f
tab-oo <tab-oo@users.noreply.github.com>
2022-06-13 14:36:19 +0000

    deploy: 1af49b1c4683c26425459efbebfd7ffe6313185b

--- _nuxt/static/1648130562/es/imprint/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648130562",layout:"default",error:a,serverRendered:true,routePath:"\u002Fes\u002Fimprint",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5e26d92c"}}}}(null));
--- _nuxt/static/1648130562/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Far\u002Fdraft","\u002Fen\u002Fdraft","\u002Fes\u002Fdraft","\u002Fdraft","\u002F","\u002Fprivacy","\u002Fimprint","\u002Far\u002Fimprint","\u002Far\u002Fprivacy","\u002Fen\u002Fimprint","\u002Fen\u002Fprivacy","\u002Fes\u002Fimprint","\u002Fes\u002Fprivacy","\u002Fes","\u002Fen","\u002Far"]})
