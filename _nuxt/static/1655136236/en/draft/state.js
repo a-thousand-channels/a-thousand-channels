9e3940347c5ae3d6ab9643fbae8e41283227170d
ut <ut@users.noreply.github.com>
2022-06-13 16:21:51 +0000

    deploy: 0c631fbf3166d836d12ef128d0f0edf863a3477e

--- _nuxt/static/1655136236/en/draft/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655136236",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fdraft",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fef6c81f"}}}}(null));
---
+++ _nuxt/static/1655137308/ar/draft/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655137308",layout:"default",error:a,serverRendered:true,routePath:"\u002Far\u002Fdraft",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f64213c9"}}}}(null));
