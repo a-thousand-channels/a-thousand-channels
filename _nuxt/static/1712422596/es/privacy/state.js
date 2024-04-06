2ad38f8b83d8d5cd52a001c16fd74b3b448f861c
Jarami110d <Jarami110d@users.noreply.github.com>
2024-04-06 16:56:38 +0000

    deploy: f7e7f46ed0c1f813aec7a7c4b38ad7826a59ef48

---
+++ _nuxt/static/1712422596/es/privacy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712422596",layout:"default",error:a,serverRendered:true,routePath:"\u002Fes\u002Fprivacy",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8a4643e6"}}}}(null));
