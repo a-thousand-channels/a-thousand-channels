ebd2a2bf5b20ada9cc8a0e137a3280bdaf34c7bc
ut <ut@users.noreply.github.com>
2024-04-08 09:11:29 +0000

    deploy: 1d9601eb5fe922eed1bb40f4ad205f09006bc186

--- _nuxt/static/1712422983/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fen\u002Fdraft","\u002Far\u002Fdraft","\u002Fes\u002Fdraft","\u002Fimprint","\u002Fprivacy","\u002Fdraft","\u002Fen\u002Fimprint","\u002Far\u002Fprivacy","\u002Fes\u002Fimprint","\u002Far\u002Fimprint","\u002Fen\u002Fprivacy","\u002Fes\u002Fprivacy","\u002Fes","\u002Fen","\u002Far"]})
